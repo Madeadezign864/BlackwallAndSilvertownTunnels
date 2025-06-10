94e2e1ae93724a27e764824937ccd46f8d4fcdb2
Madeadezign864 <malcolmriccardo@gmail.com>
2025-06-10 12:03:26 +0100

    Initial upload and first commit
    first commit for Blackwall and Silvertown Tunnel charge

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  function togglePopup() {
+    let popup = document.getElementById("popup-card");
+    popup.style.display = (popup.style.display === "none" || popup.style.display === "") ? "block" : "none";
+  }
+
+   function togglePopupP() {
+    let popupP = document.getElementById("popup-cardP");
+    popupP.style.display = (popupP.style.display === "none" || popupP.style.display === "") ? "block" : "none";
+  }
+
+
+
+
+  // 6.  Retrieve & Display Comments
+ 
