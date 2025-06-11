5bc23d9ca4e3bfa50681f1bdf27ccaf587438082
Madeadezign864 <malcolmriccardo@gmail.com>
2025-06-11 08:10:01 +0100

    Update js file
    Entered client id for google log in

--- script.js
+++ script.js
@@ -8,6 +8,20 @@
     let popupP = document.getElementById("popup-cardP");
     popupP.style.display = (popupP.style.display === "none" || popupP.style.display === "") ? "block" : "none";
   }
+function googleLogin() {
+    google.accounts.id.initialize({
+     client_id: "324761228669-enqfav9upo37n6ftmlfrkp50lqp4ubm4.apps.googleusercontent.com",
+      callback: handleCredentialResponse
+    });
+
+    google.accounts.id.prompt();
+  }
+
+  function handleCredentialResponse(response) {
+    console.log("ID Token: " + response.credential);
+    alert("Successfully signed in!");
+  }
+
 
 
 
